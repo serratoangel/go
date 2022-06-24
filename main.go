dcd493986291bec59a15a396d55dd30246b3ce2e
Angel Serrato <serratoariasangel@gmail.com>
2022-06-24 17:59:31 -0500

    Add class

--- main.go
+++ main.go
@@ -3,20 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// argumento 1 para que tipo de dato, 3 para longitud, 3 para la capacidad
-	slice := make([]int, 3, 3)
-	// modificar valores segun sus indices
-	slice[0] = 100
-	slice[1] = 200
-	slice[2] = 300
-	// agregar nuevos elementos a nuestro slice con append
-	slice = append(slice, 400)
-	fmt.Println(slice)
-	// len para longitud, cap para capacidad
-	fmt.Println(len(slice))
-	fmt.Println(cap(slice))
-
-
+	
 }
 
 /*
@@ -200,4 +187,150 @@ Clase 19
 	// len para longitud, cap para capacidad
 	fmt.Println(len(slice))
 	fmt.Println(cap(slice))
+
+Clase 20 
+- map coleccion desorganizada de llaves valor, se pueden ver como diccionarios
+	// las llaves son de tipo int y el tipo de dato a almacenar es string
+	dias := make(map[int]string)	
+	dias[0] = "Domingo"
+	dias[1] = "Lunes"
+	dias[2] = "Martes"
+	dias[3] = "Miercoles"
+	dias[4] = "Jueves"
+	dias[5] = "Viernes"
+	dias[6] = "Sabado"
+	// para eliminar llave
+	delete(dias, 4)
+- crear un map utilizando make
+	usuarios := make(map[string] []int)
+	usuarios["Angel"] = []int { 1, 2 ,3 ,4 ,5}
+
+Clase 21
+- iterar un map con un for each
+	usuarios := map[int] string {}
+
+	usuarios[1] = "Usuario 1"
+	usuarios[2] = "Usuario 2"
+	usuarios[3] = "Usuario 3"
+	usuarios[4] = "Usuario 4"
+
+	for llave, valor := range usuarios {
+		fmt.Println(llave, valor)
+	}
+
+Clase 21
+- condicionales
+func main() {
+	var edad int
+	fmt.Println("Ingresa tu edad: ")
+	fmt.Scanf("%d", &edad)
+	if edad >= 18 {
+		fmt.Println("Es mayor de edad")
+	} else {
+		fmt.Println("Es menor de edad")
+	}
+}
+
+Clase 22
+- multiples condicionales
+func main() {
+	var grade int
+	fmt.Println("Ingrese calificacion: ")
+	fmt.Scanf("%d", &grade)
+	if grade == 10 {
+		fmt.Println("Perfecto")
+	} else {
+		if grade == 8 || grade == 9 {
+			fmt.Println("Aprobaste la materia")
+		} else {
+			if grade == 7 || grade == 6 {
+				fmt.Println("Estudia mas")
+			} else {
+				if grade >= 0 && grade <= 5 {
+					fmt.Println("No aprobaste")
+				}
+			}
+		}
+	}
+}
+- otra forma de hacer lo mismo
+if grade == 10 {
+		fmt.Println("Perfecto")
+	} else if grade == 8 || grade == 9 {
+		fmt.Println("Aprobaste la materia")
+	} else {
+		fmt.Println("Eso")
+	}
+
+Clase 23
+- declarar e iniciar variables usando if
+func main() {
+	// las variables creadas dentro del if no se pueden utilizar afuera por el scope
+	if nombre, edad := "Cody", 7; nombre == "Cody" {
+		fmt.Println("hola, ", nombre, "bienvenido")
+	} else {
+		fmt.Println("Los valores", nombre, edad)
+	}
+}
+
+Clase 24
+- Switch
+func main() {
+	switch {
+	case grade == 10:
+		fmt.Println("Aprobado")
+	case grade == 8 || grade == 9:
+		fmt.Println("Yes")
+	default:
+		fmt.Println("Predeterminado")
+	}
+}
+- otra forma
+func main() {
+	switch grade {
+	case 10:
+		fmt.Println("Aprobado")
+	case 8, 9:
+		fmt.Println("Aprobado")
+	default:
+		fmt.Println("Aprobado")
+	}
+}
+
+Clase 25
+- obtener valores de un mapa
+func main() {
+	usuarios := make(map[string]string)
+	usuarios["Angel"] = "serrato@gmail.com"
+	correo, booleano := usuarios["Angel"]
+	if booleano {
+		fmt.Println(correo)
+	} else {
+		fmt.Println("No hay valor")
+	}
+}
+
+Clase 26 
+- ciclo for
+func main() {
+	// numeros pares del 1 al 100
+	for i := 1; i <= 100; i++ {
+		if i % 2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+Clase 27
+- ciclo for usado como while, go no soporta el ciclo while
+func main() {
+	// cantidad de digitos que posee un numero
+	numero := 123456
+	contador := 0
+	for numero > 0 {
+		numero = numero / 10
+		contador ++
+	}
+	fmt.Println("La cantidad de digitos es ", contador)
+}
 */
