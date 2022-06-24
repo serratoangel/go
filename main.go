5baae274f96db38af42b1f41e8bfef12fdb98eb6
Angel Serrato <serratoariasangel@gmail.com>
2022-06-23 19:45:43 -0500

    Add notes

--- main.go
+++ main.go
@@ -2,7 +2,8 @@
 Junio 23 2022
 Código Facilito
 Clase 3
-- conocer la version de go
+
+- version go
 go version
 - nombre paquete
 package main
@@ -16,17 +17,57 @@ fmt.Println("Hola mundo!!!")
 go build main.go
 - correr sin necesidad de compilar
 go run main.go
+
 */
+
+// Otra forma de comentar código
 
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo!!!")
+	var nombre string
 }
 
 /*
+Junio 23 2022
+Código Facilito
+Clase 4
 
+- declarar variables
+var nombre string => tipo string se inicia como vacio
+var edad int => tipo entero se inicia como cero
+- no se puede cambiar el tipo de dato de la variable
+var nombre string
+var edad int
+var name string = "Angel Serrato"
+nombre = "Eduardo Ismael"
+edad = 35
+fmt.Println(nombre)
+fmt.Println(edad)
+fmt.Println(name)
 */
 
+
+/*
+Junio 23 2022
+Código Facilito
+Clase 5, 6
+
+- se puede crear la variable sin el tipo de dato
+nombre := "Angel Serrato"
+edad := 35
+var altura = 1.83 valor float
+- para imprimir el valor de varias variables en consola, solo se separa con una coma
+fmt.Println(nombre, edad, altura)
+fmt.Println("El nombre es:", nombre, edad, altura)
+*/
+
+/*
+Junio 23 2022
+Código Facilito
+Clase 7
+
+- declarar varias variables
+*/
