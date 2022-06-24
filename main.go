1833848eae3843c968eb7b07cdfe2cfa96a37883
Angel Serrato <serratoariasangel@gmail.com>
2022-06-23 19:25:18 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+/*
+Junio 23 2022
+Código Facilito
+Clase 3
+- conocer la version de go
+go version
+- nombre paquete
+package main
+- imprimir en consola
+import "fmt"
+- funcion principal, debe ser main y no recibe parametros
+func main()
+- imprimir con salto de linea
+fmt.Println("Hola mundo!!!")
+- compilar un programa, main.go se refiere al nombre del programa en este caso
+go build main.go
+- correr sin necesidad de compilar
+go run main.go
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hola mundo!!!")
+}
+
+/*
+
+*/
+
